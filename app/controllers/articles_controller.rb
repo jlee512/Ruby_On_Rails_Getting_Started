fc61cdcb41cba695c968c8c07fd1668a94b6747b
jlee512 <jlee512@aucklanduni.ac.nz>
2017-08-24 16:14:05 +1200

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  # Ordering of methods is important - originally not working as this was below the 'private' keyword
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  # Article parameters definition extracted for re-use
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
